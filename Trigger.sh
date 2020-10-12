3b361323241ddbd8bfc251ab91c4de4c24145607
SSAzSeed <72390388+SSAzSeed@users.noreply.github.com>
2020-10-12 17:05:36 +0530

    Update Trigger.sh

--- Trigger.sh
+++ Trigger.sh
@@ -1,1 +1,1 @@
-az pipelines run --branch trigger_test_3 --name developer_daytrader --org https://dev.azure.com/AzSeedTeam/ -p AzSeed
+az pipelines run --branch trigger_test_4 --name developer_daytrader --org https://dev.azure.com/AzSeedTeam/ -p AzSeed
