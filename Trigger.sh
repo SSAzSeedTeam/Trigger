bbe7c74edc94fe05bfb87afc0408f33956e4d6fe
SSAzSeed <72390388+SSAzSeed@users.noreply.github.com>
2020-10-12 15:14:48 +0530

    Update Trigger.sh

--- Trigger.sh
+++ Trigger.sh
@@ -1,3 +1,1 @@
-echo ${AZURE_DEVOPS_CLI_PAT} | az devops login
-az devops configure --defaults organization=https://dev.azure.com/AlphaGX project="GXEnablement" --use-git-aliases true
 az pipelines run --branch Trigger_test_2 --name SSAzSeedTeam.daytrader-MultiStage --org https://dev.azure.com/AzSeedTeam/ -p AzSeed
