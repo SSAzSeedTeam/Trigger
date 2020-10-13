61e88b95dc0cd3f4d9918f20a32ee97889c5ff43
SSAzSeed <72390388+SSAzSeed@users.noreply.github.com>
2020-10-13 17:54:06 +0530

    Update Trigger.sh

--- Trigger.sh
+++ Trigger.sh
@@ -1,1 +1,1 @@
-az pipelines run --branch kartik_ConfluentCloudKafka --name developer_daytrader --org https://dev.azure.com/AzSeedTeam/ -p AzSeed 
+az pipelines run --branch trigger_demo --name developer_daytrader --org https://dev.azure.com/AzSeedTeam/ -p AzSeed 
