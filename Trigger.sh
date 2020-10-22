63007154c1cc24589d5c07f7f22b33a4e22473cd
Shibu TN <tnshibu@yahoo.com>
2020-10-22 19:56:32 +0530

    a

--- Trigger.sh
+++ Trigger.sh
@@ -1,2 +1,2 @@
-az pipelines run --branch master --name developer_daytrader --org https://dev.azure.com/AzSeedTeam/ -p AzSeed 
+az pipelines run --branch shibu_chaos_testing --name developer_daytrader --org https://dev.azure.com/AzSeedTeam/ -p AzSeed 
 
