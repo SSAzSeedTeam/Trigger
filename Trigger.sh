e373de9b42f11b73e61e3b5accab762fea34037a
Shibu TN <tnshibu@yahoo.com>
2020-11-06 16:55:44 +0530

    jsjhsa

--- Trigger.sh
+++ Trigger.sh
@@ -1,2 +1,2 @@
-az pipelines run --branch shibu_chaos_testing-2 --name AzSeedTeam.Developer --org https://dev.azure.com/AzSeedTeam/ -p AzSeed  
+az pipelines run --branch shibu_chaos_testing-2 --name AzSeedTeam.Developer --org https://dev.azure.com/AzSeedTeam/ -p AzSeed       
 
